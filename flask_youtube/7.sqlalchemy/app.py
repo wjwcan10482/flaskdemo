59a24c8fd9568e50469bc371839e1a484d786171
wangjinm <wjwcan10482@163.com>
2020-05-20 17:37:14 +0800

    update

--- flask_youtube/7.sqlalchemy/app.py
+++ flask_youtube/7.sqlalchemy/app.py
@@ -2,21 +2,21 @@ from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+
 app = Flask(__name__) 
+app.config.from_object(Config)
+
 bootstrap=Bootstrap(app)
 db=SQLAlchemy(app)
 
-app.config.from_object(Config)
-
 @app.route('/')
 def index():
-    paragraphs= [
-        'Section 1',
-        'Section 2',
-        'Section 3',
-    ]
-    return render_template('index.html',title='Home',data=paragraphs)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=3000)
+
+
+# from models import User
+# User.query.all()
